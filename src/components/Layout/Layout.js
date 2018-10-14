3e9bfba32c68fe020b6829e1460c8c7bd7275748
Purush Kuppan <purush.kuppan@gmail.com>
2018-10-14 02:17:58 -0500

    Burger project

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from './Layout.css'
 const Layout = (probs) => (
 <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.content}>
+    <main className={classes.Content}>
     {probs.children}
         </main>
     </>
